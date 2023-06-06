17e791ebd0855a900042d7ffebbc004a41dfea5e
kosh202 <80370866+kosh202@users.noreply.github.com>
2023-06-05 21:09:20 -0300

    atualizado e funcional

--- app/src/main/java/com/example/listacompras/MainActivity.kt
+++ app/src/main/java/com/example/listacompras/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.listacompras
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.listacompras.databinding.ActivityMainBinding
+import com.example.listacompras.databinding.ItemBinding
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -61,9 +62,22 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun configurarBase(){
+    fun configurarBase(){//configurar firebase
         FirebaseAuth.getInstance().currentUser?.let {
             database = FirebaseDatabase.getInstance().reference.child(it.uid)
+
+            val valueEventListener = object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    tratarDadosProdutos(snapshot)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w("MainActivity", "configurarBase", error.toException())
+                    Toast.makeText(this@MainActivity, "Erro de conexão", Toast.LENGTH_LONG).show()
+                }
+
+            }
+            database.child("produtos").addValueEventListener(valueEventListener)
         }
     }
 
@@ -71,7 +85,74 @@ class MainActivity : AppCompatActivity() {
         val editText = EditText(this)
         editText.hint = "nome do Item"
 
-        AlertDialog.Builder(this).setTitle("Novo Item").setView(editText)
-            .setPositiveButton("Inserir", null).create().show()
+        AlertDialog.Builder(this)
+            .setTitle("Novo Item")
+            .setView(editText)
+            .setPositiveButton("Inserir"){dialog, button->
+                val produto = Produto(nome = editText.text.toString())
+                val novoNo = database.child("produtos")//definido o caminho
+                    .push()//cria novo no
+
+                produto.id = novoNo.key
+                novoNo.setValue(produto)
+
+            }.create()
+            .show()
+    }
+
+    fun tratarDadosProdutos(dataSnapshot: DataSnapshot){
+        val listaProdutos = arrayListOf<Produto>()
+
+        dataSnapshot.children.forEach{
+            val produto = it.getValue(Produto::class.java)
+
+            /*
+            if (produto != null){
+                listaProdutos.add(produto)
+
+            }
+            * */
+            produto?.let {
+                listaProdutos.add(it)
+
+            }
+        }
+        atuakizarTela(listaProdutos)
+
+
+    }
+
+    fun atuakizarTela(lista: List<Produto>){
+
+        binding.conteiner.removeAllViews()
+
+        lista.forEach {
+            //infla o elemento que representa 1 item
+            val item = ItemBinding.inflate(layoutInflater)
+            //configura os atributos no elemento
+            item.nome.text = it.nome
+            item.comprado.isChecked = it.comprado
+
+            item.excluir.setOnClickListener{ view->
+                it.id?.let{
+                    val no = database.child("produtos").child(it)
+                    no.removeValue()
+                }
+
+            }
+
+            item.comprado.setOnCheckedChangeListener{button, isChecked ->
+                it.id?.let{
+                    val no = database.child("produtos").child(it)
+                    no.child("comprado").setValue(isChecked)
+
+                }
+
+            }
+
+
+            //coloca o  elemento dentro do conteiner
+            binding.conteiner.addView(item.root)
+        }
     }
 }
---
+++ app/src/main/java/com/example/listacompras/Produto.kt
@@ -0,0 +1,7 @@
+package com.example.listacompras
+
+data class Produto(
+    var id: String? = null,
+    var nome: String = "",
+    var comprado: Boolean = false
+)
